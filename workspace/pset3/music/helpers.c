82ee9aa7dd9dbb873e9f03b3dcc371da2f1273eb
davetormey <dave.tormey@gmail.com>
2018-06-23 12:33:32 -0700

    Music

---
+++ workspace/pset3/music/helpers.c
@@ -0,0 +1,30 @@
+// Helper functions for music
+
+#include <cs50.h>
+#include <stdio.h>
+
+#include "helpers.h"
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+    // TODO
+    printf("%s\n", fraction);
+    return 0;
+}
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+    printf("%s\n",note );
+    return 0;
+}
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+    // TODO
+    printf("%s\n",s );
+    return 0;
+}
